cbf5fa3823b9e7edd0d64df6e45e20050367a07d
danielqv92 <danielqv0292@gmail.com>
2018-12-14 17:22:52 -0600

    imprime coordenada del puntero del mouse sobre el video

--- deteccion_en_area_especifica.py
+++ deteccion_en_area_especifica.py
@@ -1,43 +1,60 @@
 # coding=utf-8
 """
-Fecha:13 de diciembre del 2018
+Fecha:14 de diciembre del 2018
 
 @author: Daniel Quesada Vindas
 
 @ITCR_sede_San_Carlos
+
+@Logros:se imprime la coordenada del puntero del mouse sobre el video
+@Notas: El video esta enciclado para poder trabajar con un demo
+@Siguiente paso: detectar en espacio especifico
 """
 #Headers a utilizar:
 import cv2
 import time
 import numpy as np
-import argparse         #para analizar linea de comandos
-
-#Inicializa la lista de puntos de referencia y boleanos, indicando si el recorte se esta realizando o no...
-refPuntero = []          #para guardas las dos coordenadas del click del mouse
-click_recorte = False    #para indicar si el click izq del mouse esta presionado...
-
-#Argumentos relacionados con la funcion click_y_recortar:
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", )
-
 
 #Crear el clasificador para automoviles
 clasificador_autos = cv2.CascadeClassifier('Haarcascades/cars.xml')
 
+#Variables globales para la funcion dibujar_rect_mouse
+dibujando = False
+punto1 = ()
+punto2 = ()
+
+
+#Funcion que 
+def coordenada_puntero(evento, x, y, banderas, parametros):
+    global punto1, punto2, dibujando
+    #if evento == cv2.EVENT_LBUTTONDOWN:
+    #    if dibujando is False:
+    #        dibujando = True
+    #        punto1 = (x,y)
+    #    else:
+    #        dibujando = False
+    #elif evento == cv2.EVENT_MOUSEMOVE:
+    #    if dibujando is True:
+    #        punto2 = (x,y)
+   
+    if evento == cv2.EVENT_MOUSEMOVE: 
+       print "X: {} || Y: {}".format(x,y)
+
+cv2.namedWindow('Cuadro')
+cv2.setMouseCallback('Cuadro', coordenada_puntero)
+
 #Iniciar video captura de un archivo de video para pruebas
 captura = cv2.VideoCapture('images/cars.avi')
 
-
-
-
 #Si el video abre correctamente se inicia el siguiente loop
-"""
+
 while captura.isOpened():
 
     #time.sleep(.05)            #time sleep para ver el video más lento
     # Lee el primer cuadro:
     ret, cuadro = captura.read() #captura cuadro por cuadro
-    if ret:
+    
+    """if ret:
         gris = cv2.cvtColor(cuadro, cv2.COLOR_BGR2GRAY)
 
         # Pasa el cuadro por nuestro clasificador de autos
@@ -49,43 +66,30 @@ while captura.isOpened():
             
             cv2.imshow('Autos', cuadro)
             cv2.waitKey()
+    """
+    #if punto1 and punto2:
+    #    cv2.rectangle(cuadro, punto1, punto2, (0, 255, 0),2)
+    
+    #Si hay captura entonces ret=True
+    if ret:
+        cv2.imshow('Cuadro', cuadro)
+        #cv2.waitKey()
+        
+    #Si el video se termina    
+    elif ret==False:    
+        captura = cv2.VideoCapture('images/cars.avi')   #Descomentar para enciclar el video
+        #break                                          #Descomentar y comentar linea anterior para quebrar el while cuando termina el video...
+            
 
     if cv2.waitKey(1) == 13: #13 is the Enter Key
-            break
-"""
-if captura.isOpened():
-    ret, cuadro = captura.read() #captura cuadro por cuadro
-    cv2.imshow('Autos', cuadro)
-    cv2.waitKey()
+        break
 
-#fuente de referencia:
-#https://www.pyimagesearch.com/2015/03/09/capturing-mouse-click-events-with-python-and-opencv/
-def click_y_recortar(evento, x, y, banderas, parametros):
+print "Se cerro el video"
+
+
+
+
     
-    global refPuntero, click_recorte       #variables globales
-    
-    
-    #si el click izq del mouse se presiona, da la direccion del punto inicial (x,y)
-    #e indica que el recorte va a empezar
-    if evento == cv2.EVENT_LBUTTONDOWN:
-        refPuntero = [(x, y)]
-        click_recorte = True    #cambia el estado de recorte a Verdadero (inicia aqui...)
-    
-    #Revisa si el boton izq del mouse se dejo de presionar...
-    elif evento == cv2.EVENT_LBUTTONUP:
-        #Graba el final de las coordenadas (x,y) e indica que la operacion de recorte finalizo
-        refPuntero.append((x,y))
-        click_recorte = False   #cambia el estado de recorte a Falso... deja de recortar
-        
-        #dibujar rectangulo alrededor dela region de interes:
-        cv2.rectangle(cuadro, refPuntero[0], refPuntero[1], (0, 255, 0), 2)
-        
-        
-
-#Captura solo el primer cuadro y congela la imagen hasta que se presione un tecla
-
-
-
 
 
 captura.release()
