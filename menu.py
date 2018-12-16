f18f8dedb43054ea96c9b23a147c19610273644a
danielqv92 <danielqv0292@gmail.com>
2018-12-15 20:18:02 -0600

    menu con barra de progreso--se cambiara  a otra para probar otra manera

--- menu.py
+++ menu.py
@@ -9,6 +9,11 @@ Fecha:15 de diciembre del 2018
 Menu principal
 """
 
+import time
+import sys
+
+
+print'\n\n'
 print'                                 _________'
 print'                          _.--\"\"\'-----,   `\"--.._'
 print'                       .-\'\'   _/_      ; .\'\"----,`-,'
@@ -28,7 +33,43 @@ print'   `|TECNOLABS|__;_...--\'\": :  \    //  |---\"\"\"      \__\_/'
 print'     \"\"\"\"\"\"\"\"\"\'            \ \  \_.//  /'
 print'       `---\'                \ \_     _\''
 print'                             `--`---\'  '
-print'\n\n\n\n\n\n\n\n'
-print'hola123'
 
+print'\n'
+
+print'________                .__       .__   '
+print'\______ \ _____    ____ |__| ____ |  |  '
+print' |    |  \\__   \  /    \|  |/ __ \|  |  '
+print' |    `   \/ __ \|   |  \  \  ___/|  |__'
+print'/_______  (____  /___|  /__|\___  >____/'
+print'        \/     \/     \/        \/      '
+
+print'________                                    .___       '
+print'\_____  \  __ __   ____   ___________     __| _/____   '
+print' /  / \  \|  |  \_/ __ \ /  ___/\__  \   / __ |\__  \  '
+print'/   \_/.  \  |  /\  ___/ \___ \  / __ \_/ /_/ | / __ \_'
+print'\_____\ \_/____/  \___  >____  >(____  /\____ |(____  /'
+print'       \__>           \/     \/      \/      \/     \/ '
+
+print'____   ____.__            .___              '
+print'\   \ /   /|__| ____    __| _/____    ______'
+print' \   Y   / |  |/    \  / __ |\__  \  /  ___/'
+print'  \     /  |  |   |  \/ /_/ | / __ \_\___ \ '
+print'   \___/   |__|___|  /\____ |(____  /____  >'
+print'                   \/      \/     \/     \/ '
+print'\n'
+
+ancho_barra = 100		#barra de progreso
+
+# setup toolbar
+sys.stdout.write("[%s]" % ("-" * ancho_barra))  #el simbolo que va en sobrescribiendo...
+sys.stdout.flush()
+sys.stdout.write("\b" * (ancho_barra+1)) # return to start of line, after '['
+
+for i in xrange(ancho_barra):
+    time.sleep(0.1) # do real work here
+    # update the bar
+    sys.stdout.write("|")
+    sys.stdout.flush()
+
+sys.stdout.write("\n")
 
