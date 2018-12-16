5cb6854f9dc7cf7a4968fbaddc198c48a9b14223
danielqv92 <danielqv0292@gmail.com>
2018-12-15 21:46:18 -0600

    base del menu terminado--siguiente: llamar el menu del main.py y fusionar...

--- menu.py
+++ menu.py
@@ -10,7 +10,8 @@ Menu principal
 """
 
 import time
-import sys
+import os               #para funciones del sistema (linux en este caso)--> limpiar pantalla se usara
+from tqdm import tqdm   #para la barra de progreso--pip install tqdm--repositorio en github
 
 
 print'\n\n'
@@ -35,21 +36,21 @@ print'       `---\'                \ \_     _\''
 print'                             `--`---\'  '
 
 print'\n'
-
+time.sleep(0.33)
 print'________                .__       .__   '
 print'\______ \ _____    ____ |__| ____ |  |  '
 print' |    |  \\__   \  /    \|  |/ __ \|  |  '
 print' |    `   \/ __ \|   |  \  \  ___/|  |__'
 print'/_______  (____  /___|  /__|\___  >____/'
 print'        \/     \/     \/        \/      '
-
+time.sleep(0.33)
 print'________                                    .___       '
 print'\_____  \  __ __   ____   ___________     __| _/____   '
 print' /  / \  \|  |  \_/ __ \ /  ___/\__  \   / __ |\__  \  '
 print'/   \_/.  \  |  /\  ___/ \___ \  / __ \_/ /_/ | / __ \_'
 print'\_____\ \_/____/  \___  >____  >(____  /\____ |(____  /'
 print'       \__>           \/     \/      \/      \/     \/ '
-
+time.sleep(0.33)
 print'____   ____.__            .___              '
 print'\   \ /   /|__| ____    __| _/____    ______'
 print' \   Y   / |  |/    \  / __ |\__  \  /  ___/'
@@ -57,19 +58,61 @@ print'  \     /  |  |   |  \/ /_/ | / __ \_\___ \ '
 print'   \___/   |__|___|  /\____ |(____  /____  >'
 print'                   \/      \/     \/     \/ '
 print'\n'
+time.sleep(0.33)
 
-ancho_barra = 100		#barra de progreso
+#Barra de progreso que realmente no sirve para nada...
+for i in tqdm(range(100)):
+    time.sleep(0.01)
+    
+print'\n\n'
 
-# setup toolbar
-sys.stdout.write("[%s]" % ("-" * ancho_barra))  #el simbolo que va en sobrescribiendo...
-sys.stdout.flush()
-sys.stdout.write("\b" * (ancho_barra+1)) # return to start of line, after '['
+nada = raw_input("Presione la tecla Enter para continuar...")
 
-for i in xrange(ancho_barra):
-    time.sleep(0.1) # do real work here
-    # update the bar
-    sys.stdout.write("|")
-    sys.stdout.flush()
+    
+#limpiar pantalla para mostar el menu...
 
-sys.stdout.write("\n")
+def func_menu():
+    os.system('clear')
+    #Barra de progreso que realmente no sirve para nada...
+    for i in tqdm(range(20)):
+        time.sleep(0.05)
+    os.system('clear')
+    print'               ___  ___ _____ _   _ _   _                 '
+    print'               |  \/  ||  ___| \ | | | | |                '
+    print'               | .  . || |__ |  \| | | | |                '
+    print'               | |\/| ||  __|| . ` | | | |                '
+    print'               | |  | || |___| |\  | |_| |                '
+    print'               \_|  |_/\____/\_| \_/\___/                 '
+    print'\n'                                                          
+    print'______                     _                _             '
+    print'| ___ \                   (_)              | |            '
+    print'| |_/ /_ _ _ __ __ _ _   _ _ _ __ ___   ___| |_ _ __ ___  '
+    print'|  __/ _` | \'__/ _` | | | | | \'_ ` _ \ / _ \ __| \'__/ _ \ '
+    print'| | | (_| | | | (_| | |_| | | | | | | |  __/ |_| | | (_) |'
+    print'\_|  \__,_|_|  \__, |\__,_|_|_| |_| |_|\___|\__|_|  \___/ '
+    print'                  | |                                     '
+    print'                  |_|                                     '
+    print'\n\n'
+    time.sleep(0.33)
+    print' ____ ____ ____ ____ ____ ____ ____ ____ ____ '
+    print'||O |||p |||c |||i |||o |||n |||e |||s |||: ||'
+    print'||__|||__|||__|||__|||__|||__|||__|||__|||__||'
+    print'|/__\|/__\|/__\|/__\|/__\|/__\|/__\|/__\|/__\|'
+    print'\n'
+    time.sleep(0.33)
+    print'1. Mostrar video en tiempo real'
+    time.sleep(0.33)
+    print'2. Configuracion de parametros de los espacios (Obligatorio)'
+    time.sleep(0.33)
+    print'3. Mostrar video en tiempo real'
+    time.sleep(0.33)
+    print'\n'
+    
+   
 
+
+
+
+#while true 
+
+func_menu()
